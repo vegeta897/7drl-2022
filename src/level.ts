cd4338ceb595ec3fdf0eec52f1a0f3028012a575
Devin Spikowski <vegeta897@gmail.com>
2022-03-07 21:11:50 -0500

    :bento: Update sprites

--- src/level.ts
+++ src/level.ts
@@ -5,6 +5,7 @@ import { WorldSprites } from './pixi'
 import { getDiamondAround, getSquareAround, Vector2 } from './vector2'
 import Dijkstra from 'rot-js/lib/path/dijkstra'
 import { GridMap } from './map'
+import { RNG } from 'rot-js'
 
 const MAP_WIDTH = 80
 const MAP_HEIGHT = 80
@@ -16,40 +17,50 @@ export let OpenFloors: Vector2[] = []
 export let OpenWaters: Vector2[] = []
 
 export function createLevel() {
+  EntityMap = new GridMap()
+
   const walls = new ROT.Map.Cellular(MAP_WIDTH, MAP_HEIGHT)
   walls.randomize(0.5)
   for (let i = 0; i < 2; i++) {
     walls.create()
   }
   Level = new GridMap(Tile.Floor)
-  EntityMap = new GridMap()
-  const wallTexture = Texture.from('wall')
   walls.connect((x, y, value) => {
     const isBoundary = x === 0 || x === MAP_WIDTH - 1 || y === 0 || y === MAP_HEIGHT - 1
-    if (!isBoundary && value === 1) return
-    Level.set({ x, y }, Tile.Wall)
-    const wallSprite = new Sprite(wallTexture)
-    wallSprite.x = x * TILE_SIZE
-    wallSprite.y = y * TILE_SIZE
-    WorldSprites.addChild(wallSprite)
+    const tileType = !isBoundary && value === 1 ? Tile.Floor : Tile.Wall
+    Level.set({ x, y }, tileType)
   }, 1)
   const water = new ROT.Map.Cellular(MAP_WIDTH, MAP_HEIGHT)
   water.randomize(0.45)
-  const waterTexture = Texture.from('water')
   for (let i = 0; i < 3; i++) {
     water.create()
   }
   water.create((x, y, value) => {
     if (value === 0) return
-    if (Level.has({ x, y })) return
+    if (Level.get({ x, y }) === Tile.Wall) return
     Level.set({ x, y }, Tile.Water)
-    const waterSprite = new Sprite(waterTexture)
-    waterSprite.x = x * TILE_SIZE
-    waterSprite.y = y * TILE_SIZE
-    WorldSprites.addChild(waterSprite)
   })
-  for (let x = 2; x < MAP_WIDTH - 3; x++) {
-    for (let y = 2; y < MAP_HEIGHT - 3; y++) {
+
+  const wallTexture = Texture.from('wall')
+  const floorTextures = ['floor1', 'floor2', 'floor3', 'floor4'].map((t) => Texture.from(t))
+  const waterTexture = Texture.from('water')
+  const getTileTexture = (tile: Tile) => {
+    switch (tile) {
+      case Tile.Floor:
+        return RNG.getItem(floorTextures)!
+      case Tile.Wall:
+        return wallTexture
+      case Tile.Water:
+        return waterTexture
+    }
+  }
+  for (let x = 0; x < MAP_WIDTH; x++) {
+    for (let y = 0; y < MAP_HEIGHT; y++) {
+      const tileType = Level.get({ x, y })
+      const tileSprite = new Sprite(getTileTexture(tileType))
+      tileSprite.x = x * TILE_SIZE
+      tileSprite.y = y * TILE_SIZE
+      WorldSprites.addChild(tileSprite)
       const diamond2 = getDiamondAround({ x, y }, 2)
       if (diamond2.every((g) => !Level.get(g))) {
         OpenFloors.push({ x, y })
