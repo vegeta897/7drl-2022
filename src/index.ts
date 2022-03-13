4e4ba0d06c6a50ccb3191f25e93082b6f1996e20
Devin Spikowski <vegeta897@gmail.com>
2022-03-13 16:04:03 -0400

    :mute: Remove logging level change

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,6 @@ export let CurrentLevel: number
 export const LastLevel = 3
 export const nextLevel = () => {
   CurrentLevel++
-  console.log('moving to level', CurrentLevel)
   resetNonPlayerEntities()
   startLevel()
 }
